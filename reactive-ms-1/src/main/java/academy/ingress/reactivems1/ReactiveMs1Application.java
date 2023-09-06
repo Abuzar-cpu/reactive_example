adb9f4eacd615c0764f540dcfee10e49ccb7d0b3
Abuzar-cpu <kerimovabuzer25@gmail.com>
2023-09-06 07:17:34 +0400

    initial commit

---
+++ reactive-ms-1/src/main/java/academy/ingress/reactivems1/ReactiveMs1Application.java
@@ -0,0 +1,13 @@
+package academy.ingress.reactivems1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMs1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMs1Application.class, args);
+    }
+
+}
---
+++ reactive-ms-1/src/test/java/academy/ingress/reactivems1/ReactiveMs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.ingress.reactivems1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMs1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
